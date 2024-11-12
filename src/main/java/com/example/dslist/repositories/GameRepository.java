58347b7a28377eb2fef81dbbf2d566b91de3c71a
henriqueacf <henrique.assis145@gmail.com>
2024-11-12 10:34:03 -0300

    feat: reiniciando projeto

--- src/main/java/com/example/dslist/repositories/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dslist.repositories;
-
-import com.example.dslist.entities.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
